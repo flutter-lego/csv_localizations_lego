33033c006dd97f1964eb83a687b9a73941ed9473
june lee <melodysdreamj@gmail.com>
2024-07-31 16:29:16 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/csv_localizations_lego/_.dart
@@ -0,0 +1,24 @@
+import 'package:csv_localizations/csv_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+
+import '../../../../../../main.dart';
+import '../../../../../config/csv_localizations_lego/_.dart';
+import '../../../../shared_params/_/material_app.dart';
+
+@ReadyBeforeMaterialApp()
+Future<void> readyForCsvLocalizationsLego(BuildContext context) async {
+  MaterialAppParams.localizationsDelegates = [
+    // delegate from flutter_localization
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    // delegate from csv_localizations
+    CsvLocalizationsDelegate(path: 'assets/lego/csv_localizations_lego/translations.csv'),
+  ];
+
+  MaterialAppParams.supportedLocales = supportLanguage;
+
+
+}
